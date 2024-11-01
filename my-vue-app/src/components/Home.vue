2d1ed086d3f14e5d6cd67ef6c10a6709e5959764
calvin-k347 <clark.calvink@gmail.com>
2024-11-01 11:47:20 -0400

    routing

---
+++ my-vue-app/src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="banner">
+  <div class="banner_child">
+    <p class="subtitle">Why</p>
+  </div>
+  <div class="banner_child">
+    <p class="subtitle">LinkedIn</p>
+  </div>
+  <div class="banner_child">
+    <p class="subtitle">Github</p>
+  </div>
+</div>
+<div class="section">
+  <p class="subtitle">Phonology</p>
+</div>
+<div class="section">
+  <p class="subtitle">Syntax</p>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #24496b;
+}
+#banner {
+  width: 100vw;
+  height: 5vh;
+  background-color: #f5e050;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.banner_child {
+  display: inline;
+  margin: 10vw;
+}
+.subtitle {
+  color: #24496b;
+  display: inline;
+  padding-top: 10%;
+}
+.banner_child:hover {
+  cursor: pointer;
+}
+.section {
+  display: inline;
+  margin: 10vh;
+  padding: 2.5vh;
+  background-color: #f5e050;
+}
+.section:hover {
+  background-color: #f4e376;
+  cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from './components/Home.vue'
+import Syntax from './components/syntax.vue'
+const routes = [
+  { path: '/', component: Home },
+  { path: '/syntax', component: Syntax}
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+createApp(App)
+.use(router)
+.mount('#app')
+
